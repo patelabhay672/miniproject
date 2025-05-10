708f1dc33ecc11a55feec45bca82ccb855deb556
patelabhay672 <patelabhay0302@gmail.com>
2025-05-10 22:27:06 +0530

    Adding multer and move to next

--- app.js
+++ app.js
@@ -6,41 +6,41 @@ const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
-const multer = require("multer");
-const path=require("path")
+// const multer = require("multer");
+// const path=require("path")
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/images/uploads");
-  },
-  filename: function (req, file, cb) {
-    crypto.randomBytes(12, function (err, bytes) {
-      const fn=bytes.toString("hex") + path.extname(file.originalname)
-      cb(null, fn);
-    });
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./public/images/uploads");
+//   },
+//   filename: function (req, file, cb) {
+//     crypto.randomBytes(12, function (err, bytes) {
+//       const fn=bytes.toString("hex") + path.extname(file.originalname)
+//       cb(null, fn);
+//     });
+//   },
+// });
 
-const upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
+// app.get("/", (req, res) => {
+//   res.render("index.ejs");
+// });
 
-app.get("/test", (req, res) => {
-  res.render("test");
+// app.get("/test", (req, res) => {
+//   res.render("test");
   
-});
+// });
 
-app.post("/upload", upload.single("image"),(req, res) => {
-  console.log(req.file)
-  res.send("done")
-});
+// app.post("/upload", upload.single("image"),(req, res) => {
+//   console.log(req.file)
+//   res.send("done")
+// });
 
 app.get("/login", (req, res) => {
   res.render("login.ejs");
